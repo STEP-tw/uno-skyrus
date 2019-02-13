19a25e2f7f5a1c06f11b5de966c1195631a959c9
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-02-13 22:29:01 +0530

    Modify pre_commit_hooks, pre_push_hooks and setup.sh
    Now setup.sh can configure commit template
    Move setup.sh to bin directory

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,4 +6,6 @@ chmod +x .git/hooks/pre-commit
 cp .github/pre_push_hooks .git/hooks/pre-push
 chmod +x .git/hooks/pre-push
 
+git config --local commit.template '.github/git_commit_template'
+
 npm install
