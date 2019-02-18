867107a66732e160cf7286a4a8c7d85722c3beb4
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-02-18 11:12:10 +0530

    Aftab/Reshmi - Render gamepage
    What changes have you made?
    
    * Handle url('/game.html')
    
    Why do we need this change?
    
    * So that player can see the game and his cards.
    
    What can this change impact?
    
    * It helps to serve the gamepage on url(/game.html)

--- test/functionalTest/appTest.js
+++ test/functionalTest/appTest.js
@@ -2,10 +2,21 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 describe('homepage', function() {
-  it('should return 404 status code for homepage', function(done) {
+  it('should return 200 status code for homepage', function(done) {
     request(app)
       .get('/')
-      .expect(404)
+      .expect(200)
+      .expect('content-type','text/html; charset=UTF-8')
+      .end(done);
+  });
+});
+
+describe('gamepage', function() {
+  it('should return 200 status code for gamepage', function(done) {
+    request(app)
+      .get('/game.html')
+      .expect(200)
+      .expect('content-type','text/html; charset=UTF-8')
       .end(done);
   });
 });
