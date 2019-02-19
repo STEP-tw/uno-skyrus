1afdd8a90c3194297b48eec2323679536bb70a7b
praathul <44019979+athulpram@users.noreply.github.com>
2019-02-19 11:21:48 +0530

    3 Ahtul/Bhawna - create frontend view for other players(4 players) What changes have you made?
    * create Players class
    * modify game.html to show 4 players and their cards
    
    Why do we need this change?
    
    * To show other player cards in the view
    
    What can this change impact?
    * Players can see the cards of other players (max 3)

---
+++ src/models/players.js
@@ -0,0 +1,18 @@
+class Players {
+  constructor(hostPlayer) {
+    this.host = hostPlayer;
+    this.players = [hostPlayer];
+    this.currentPlayer = hostPlayer;
+  }
+  getCurrentPlayer() {
+    return this.currentPlayer;
+  }
+  getNumberOfPlayers() {
+    return this.players.length;
+  }
+  addPlayer(player) {
+    this.players.push(player);
+  }
+}
+
+module.exports = { Players };
---
+++ test/modelTest/playersTest.js
@@ -0,0 +1,45 @@
+const { Players } = require('./../../src/models/players.js');
+const chai = require('chai');
+
+describe('addPlayer', function() {
+  it('should add a player to the Players class', function() {
+    const players = new Players({ name: 'hostName', cards: [] });
+    players.addPlayer({ name: 'player1', cards: [] });
+
+    const actualOutput = players.players;
+    const expectedOutput = [
+      { name: 'hostName', cards: [] },
+      { name: 'player1', cards: [] }
+    ];
+
+    chai.assert.deepEqual(expectedOutput, actualOutput);
+  });
+});
+
+describe('getCurrentPlayer', function() {
+  it('should return current player', function() {
+    const players = new Players({ name: 'hostName', cards: [] });
+    const expectedOutput = { name: 'hostName', cards: [] };
+    const actualOutput = players.getCurrentPlayer();
+
+    chai.assert.deepEqual(expectedOutput, actualOutput);
+  });
+});
+
+describe('getNumberOfPlayers', function() {
+  it('should return numbers of all players', function() {
+    const players = new Players({ name: 'hostName', cards: [] });
+    const expectedOutput = 1;
+    const actualOutput = players.getNumberOfPlayers();
+
+    chai.assert.deepEqual(expectedOutput, actualOutput);
+  });
+  it('should return numbers of all players after addding players', function() {
+    const players = new Players({ name: 'hostName', cards: [] });
+    players.addPlayer({ name: 'player1', cards: [] });
+    const expectedOutput = 2;
+    const actualOutput = players.getNumberOfPlayers();
+
+    chai.assert.deepEqual(expectedOutput, actualOutput);
+  });
+});
