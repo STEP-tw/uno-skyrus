3c2a2e5ff1b378a3d290402fa8c1658e7d9c8ed4
praathul <44019979+athulpram@users.noreply.github.com>
2019-02-20 20:43:26 +0530

    [#1] Athul/Reshmi - Fix bug - arranging player card
    What changes have you made?
    
    * Modify design of card
    * Arrange card of player
    
    Why do we need this change?
    
    *To show the card in a order for the player
    
    What can this change impact?
    
    *Users can see all the cards together

--- public/scripts/viewUtils.js
+++ public/scripts/viewUtils.js
@@ -3,17 +3,32 @@
 const createCard = function(document, card) {
   const unoCard = document.createElement('div');
   unoCard.className = 'uno-card';
-  unoCard.style.setProperty('background', card.color);
+
+  const numberLeft = document.createElement('div');
+  numberLeft.className = 'number-left';
+  numberLeft.innerText = card.number;
 
   const cardInner = document.createElement('div');
   cardInner.className = 'uno-card-inner';
 
   const designInner = document.createElement('div');
   designInner.className = 'uno-design-inner';
-  designInner.innerText = card.number;
+  cardInner.classList.add(card.color);
+
+  const numberSpan = document.createElement('span');
+  numberSpan.className = card.color;
+  numberSpan.innerText = card.number;
+  numberSpan.classList.add('num');
+  designInner.appendChild(numberSpan);
+
+  const numberRight = document.createElement('div');
+  numberRight.className = 'number-right';
+  numberRight.innerText = card.number;
 
   cardInner.appendChild(designInner);
+  unoCard.appendChild(numberLeft);
   unoCard.appendChild(cardInner);
+  unoCard.appendChild(numberRight);
 
   return unoCard;
 };
