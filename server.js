69990b4ac60c6cbacd8af7ba48a83f6770cce94c
Kovács Ádám <h663315@stud.u-szeged.hu>
2019-03-06 16:06:52 +0100

    Changed port to 1414 for Martin

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { app } = require('./src/app.js');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 1414;
 
 app.listen(PORT, () => {
   console.log('Listening on ' + PORT);
