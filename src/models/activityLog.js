5135e3ab8d18a5c9973b21d048e2ff30d61734f7
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2019-03-11 16:08:40 +0530

    [#35] Bhawna/Athul - Modify the model of activity log
    What changes have you made?
    
    * Modify the activity log model
    * Activity log now contains seperate methods for logging, depending on
    action
    
    Why do we need this change?
    
    * To save the statuses as a json
    
    What can this change impact?
    
    * Will only show the status as a object for now.

--- src/models/activityLog.js
+++ src/models/activityLog.js
@@ -2,11 +2,75 @@ class ActivityLog {
   constructor(logs = []) {
     this.logs = logs;
   }
-  addLog(subject, action, object) {
-    this.logs.push(subject + action + object);
-  }
+
   getLatestLog() {
     return this.logs[this.logs.length - 1];
+  }
+
+  logJoinGame(playerName) {
+    const log = {
+      action: 'join-game',
+      playerName
+    };
+    this.logs.push(log);
+  }
+
+  logLeaveGame(playerName) {
+    const log = {
+      action: 'leave-game',
+      playerName
+    };
+    this.logs.push(log);
+  }
+
+  logThrowCard(playerName, card) {
+    const log = {
+      action: 'throw-card',
+      playerName,
+      card
+    };
+    this.logs.push(log);
+  }
+
+  logDrawCards(playerName, cardCount) {
+    const log = {
+      action: 'draw-card',
+      playerName,
+      cardCount
+    };
+    this.logs.push(log);
+  }
+
+  logCallUno(playerName) {
+    const log = {
+      action: 'call-uno',
+      playerName
+    };
+
+    this.logs.push(log);
+  }
+
+  logCaught(catcher, victim) {
+    const log = {
+      action: 'caught',
+      victim,
+      catcher
+    };
+
+    this.logs.push(log);
+  }
+
+  logWrongCatch(playerName) {
+    const log = {
+      action: 'wrong-catch',
+      playerName
+    };
+
+    this.logs.push(log);
+  }
+
+  logRefillStack() {
+    this.logs.push({ action: 'refill-stack' });
   }
 }
 
