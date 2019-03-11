bcaf266a99e15d77bad3a61fe0aa0cfe04bc5245
Kovács Ádám <h663315@stud.u-szeged.hu>
2019-03-11 23:11:16 +0100

    fixed css include error

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,10 @@
 const URLS = [
   '/styles/main.css',
   '/styles/game.css',
+  '/styles/chat_style.css',
+  '/styles/chat_style2.css',
+  '/styles/w3.css',
+  '/images/back.png',
   '/scripts/game.js',
   '/scripts/viewUtils.js'
 ];
